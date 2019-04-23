0b73ae6400042fcd2effa8c8471d8e762d466453
Frank <fr.heggeness@gmail.com>
2019-04-23 09:26:55 -1000

    format

--- generator.js
+++ generator.js
@@ -2,13 +2,16 @@ module.exports = function(n) {
   // do work here
   let numberArray = [];
   let outputArray = [];
+
   for (let i = 0; i < n; i++) {
     numberArray.push(i);
   }
+
   for (let i = 0; i < n; i++) {
     let random = Math.floor(Math.random() * (n - i));
     let newNum = numberArray.splice(random, 1);
     outputArray.push(newNum);
   }
+
   return outputArray;
 };
